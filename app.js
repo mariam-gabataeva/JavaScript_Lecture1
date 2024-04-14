b14015fe44ded3d2743b5afe1d856e977a704761
mariam-gabataeva <148265916+mariam-gabataeva@users.noreply.github.com>
2024-04-14 14:17:49 +0400

    Done

---
+++ app.js
@@ -0,0 +1,13 @@
+var text = "George";
+console.log(text);
+
+let number = 6;
+console.log(number);
+
+const Sum = "Age: 27";
+console.log(Sum);
+
+const age = 27;
+const Name = "Mari";
+const sum = `My name is ${Name}. i am  ${age} years old.`;
+console.log(sum);
